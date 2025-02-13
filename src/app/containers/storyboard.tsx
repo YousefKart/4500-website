941eacc63266bd36b9c9800b5b250535f973fde5
Yousef <karty@uwindsor.ca>
2025-02-13 09:46:44 -0500

    Added Timestamps to Scenes

--- src/app/containers/storyboard.tsx
+++ src/app/containers/storyboard.tsx
@@ -13,27 +13,27 @@ export default function Storyboard() {
         {[
           {
             image: 'images/storyboard2.jpg',
-            desc: 'The screen fades in to a gray, lifeless city. Identical, self-driving box-shaped cars move silently in perfect lines, with no honking or interaction. The streets are eerily quiet.',
+            desc: '0:00 - The screen fades in to a gray, lifeless city. Identical, self-driving box-shaped cars move silently in perfect lines, with no honking or interaction. The streets are eerily quiet.',
           },
           {
             image: 'images/storyboard3.jpg',
-            desc: 'Inside a small, dimly lit garage, an old vintage car sits covered in dust—its paint chipped, its headlights dim.',
+            desc: '0:30 - Inside a small, dimly lit garage, an old vintage car sits covered in dust—its paint chipped, its headlights dim.',
           },
           {
             image: 'images/storyboard4.jpg',
-            desc: "A tiny, shiny modern car rolls in, curious but hesitant. The vintage car's headlights flicker faintly, its voice old but full of warmth.",
+            desc: "0:45 - A tiny, shiny modern car rolls in, curious but hesitant. The vintage car's headlights flicker faintly, its voice old but full of warmth.",
           },
           {
             image: 'images/storyboard5.jpg',
-            desc: "The vintage car's headlights glow brighter as the camera zooms into them, transitioning into a flashback: The vintage car speeds down a bustling street, its shiny red paint gleaming in the sunlight. Other cars honk in greeting as it passes.",
+            desc: "1:00 - The vintage car's headlights glow brighter as the camera zooms into them, transitioning into a flashback: The vintage car speeds down a bustling street, its shiny red paint gleaming in the sunlight. Other cars honk in greeting as it passes.",
           },
           {
             image: 'images/storyboard7.jpg',
-            desc: 'They navigate the monotonous, gray city streets until a winding road leads them upward. As they crest the hill, a breathtaking panorama unfolds—a rugged edge of land meeting an endless horizon drenched in the hues of a setting sun.',
+            desc: '1:30 - They navigate the monotonous, gray city streets until a winding road leads them upward. As they crest the hill, a breathtaking panorama unfolds—a rugged edge of land meeting an endless horizon drenched in the hues of a setting sun.',
           },
           {
             image: 'images/storyboard8.jpg',
-            desc: "Together, they pause, admiring the majestic view. The vintage car's once-faded red paint glows in the last rays of sunlight—a quiet, powerful testament to days gone by.",
+            desc: "1:45 - Together, they pause, admiring the majestic view. The vintage car's once-faded red paint glows in the last rays of sunlight—a quiet, powerful testament to days gone by.",
           },
         ].map((milestone, index) => (
           <div key={index}>
